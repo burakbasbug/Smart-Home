357f0a904616c5dbfbdc88da7e6d7e632d192cbf
burakbasbug <burak178@gmail.com>
2017-04-21 18:52:30 +0200

    from each device to its detail page hard coded

--- lab2/app/components/overview/overview.component.ts
+++ lab2/app/components/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
     moduleId: module.id,
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     templateUrl: './overview.component.html',
 })
 export class OverviewComponent{
-
+    
 }
