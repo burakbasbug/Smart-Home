a6b212b7dd2fcc99c7c8c837494c6673bf9a844e
Prock Alexander <asdf>
2017-05-18 17:50:40 +0200

    Server großteils fertig(Websocket und addDevice fehlen)

--- lab3/Server/server.js
+++ lab3/Server/server.js
@@ -19,12 +19,19 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // globale Variablen
-var username;
-var password;
-var devices; //Array von Devices als JSON-Objekte
-var connectedClients = [];
+var valid_username;
+var valid_password;
 
-//TODO Implementieren Sie hier Ihre REST-Schnittstelle
+var server_start_date;
+var server_start_time;
+var failed_logins = 0;
+
+var devices; //Array von Devices als JSON-Objekte
+
+var connectedClientsJWT = [];
+var privateKey = 'OURPRIVATEKEY';
+
+//T_ODO Implementieren Sie hier Ihre REST-Schnittstelle
 /* Ermöglichen Sie wie in der Angabe beschrieben folgende Funktionen:
  *  Abrufen aller Geräte als Liste
  *  Hinzufügen eines neuen Gerätes
@@ -41,52 +48,170 @@ var connectedClients = [];
  *      Bei der Anlage neuer Geräte wird eine neue ID benötigt. Verwenden Sie dafür eine uuid (https://www.npmjs.com/package/uuid, Bibliothek ist bereits eingebunden).
  */
 app.get("/deviceList", function(req, res) {
-   res.send(JSON.stringify(devices));
+    if(verifyJWT(req)) {
+        res.status(200).send(JSON.stringify(devices));
+    } else {
+        res.status(200).send("invalid_jwt");
+    }
 });
 
 app.post("/addDevice", function (req, res) {
+    if(verifyJWT(req)) {
 
+        var id = uuid();
+        var device = {
+            "id": id
+            /* TODO */
+        };
+        devices.push(device);
+        refreshConnected();
+    } else {
+        res.status(200).send("invalid_jwt");
+    }
 });
 
 app.post("/deleteDevice", function (req, res) {
+    if(verifyJWT(req)) {
+        var device = devices.filter(function (el) {
+            return el.id === req.body.id;
+        });
+        devices.remove(device);
+        res.status(200).send("successful");
+        refreshConnected();
+    } else {
+        res.status(200).send("invalid_jwt");
+    }
+});
 
+app.post("/updateDeviceName", function (req, res) {
+    if(verifyJWT(req)) {
+        var device = devices.filter(function (el) {
+            return el.id === req.body.id;
+        });
+        device.display_name = req.body.name;
+        res.status(200).send("successful");
+        refreshConnected();
+    } else {
+        res.status(200).send("invalid_jwt");
+    }
 });
 
 app.post("/updateCurrent", function (req, res) {
     "use strict";
-    //TODO Vervollständigen Sie diese Funktion, welche den aktuellen Wert eines Gerätes ändern soll
+    //T_ODO Vervollständigen Sie diese Funktion, welche den aktuellen Wert eines Gerätes ändern soll
     /*
      * Damit die Daten korrekt in die Simulation übernommen werden können, verwenden Sie bitte die nachfolgende Funktion.
      *      simulation.updatedDeviceValue(device, control_unit, Number(new_value));
      * Diese Funktion verändert gleichzeitig auch den aktuellen Wert des Gerätes, Sie müssen diese daher nur mit den korrekten Werten aufrufen.
      */
+    if(verifyJWT(req)) {
+        var device = devices.filter(function (el) {
+            return el.id === req.body.id;
+        });
+        var control_unit = device.control_units.filter(function (el) {
+            return el.name === req.body.control_unit;
+        });
+        control_unit.current = Number(req.body.new_value);
+        res.status(200);
+        simulation.updatedDeviceValue(device, control_unit, Number(req.body.new_value));
+        refreshConnected();
+    } else {
+        res.status(200).send("invalid_jwt");
+    }
 });
 
 app.post("/login", function (req, res) {
+    // read request data
+    var user = req.body.username;
+    var pwd = req.body.password;
+
+    if (user === valid_username && pwd === valid_password) {
+        console.log("login successful");
+
+        var token = jwt.sign({"username": user, "password": pwd, "time": new Date()}, privateKey);
+        connectedClientsJWT.push(token);
+        res.status(200).send(token);
+        res.status(200).send();
+    } else {
+        console.log("login failed: " + user + ": " + pwd);
+        failed_logins++;
+        res.status(200).send("invalid_login");
+    }
 
 });
 
 app.post("/logout", function (req, res) {
-
+    if(verifyJWT(req)) {
+        connectedClientsJWT.remove(req.body.token);
+        res.status(200).send("successful");
+    } else {
+        res.status(200).send("invalid_jwt");
+    }
 });
 
 app.post("/changePassword", function (req, res) {
+    if(verifyJWT(req)) {
+        var oldPwd = req.body.oldPassword;
+        var newPwd = req.body.newPassword;
+        var newPwdRep = req.body.newPasswordRep;
 
+        if(oldPwd !== valid_password) {
+            res.status(200).send("invalid, old password incorrect");
+        } else if(newPwd !== newPwdRep) {
+            res.status(200).send("invalid, new passwords dont match");
+        } else {
+            valid_password = newPwd;
+            var fileContent = "username: " + valid_username + "\npassword: " + valid_password;
+            fs.writeFile("resources/login.config", fileContent, {encoding: 'utf-8'}, function (err) {
+                if(!err) {
+                    res.status(200).send("successful");
+                } else {
+                    res.status(500).send("error writing password to file: " + err);
+                }
+            });
+        }
+    } else {
+        res.status(200).send("invalid_jwt");
+    }
 });
 
 app.get("/getServerStatus", function (req, res) {
-
+    if(verifyJWT(req)) {
+        res.status(200);
+        res.json({
+            "username": valid_username,
+            "startDate": server_start_date,
+            "startTime": server_start_time,
+            "attempts": failed_logins
+        });
+    } else {
+        res.status(200).send("invalid_jwt");
+    }
 });
+
+function verifyJWT(req) {
+    var token = req.body.token;
+    try {
+        var decoded = jwt.verify(token, privateKey);
+        // decoded.username === valid_username && decoded.password === valid_password &&
+        if(connectedClientsJWT.indexOf(req.body.token) !== -1) {
+            return true;
+        }
+    } catch(err) {
+        console.log("jwt invalid");
+    }
+    return false;
+}
 
 function readUser() {
     "use strict";
-    //TODO-fertig Lesen Sie die Benutzerdaten aus dem login.config File ein.
+    //T_ODO-fertig Lesen Sie die Benutzerdaten aus dem login.config File ein.
     fs.readFile("resources/login.config", {encoding: 'utf-8'}, function(err, data){
         if (!err) {
-            username = data.split('\n')[0].split(': ')[1];
-            console.log("read username = " + username);
-            password = data.split('\n')[1].split(': ')[1];
-            console.log("read pwd = " + password);
+            valid_username = data.split('\n')[0].split(': ')[1];
+            console.log("read username = " + valid_username);
+            valid_password = data.split('\n')[1].split(': ')[1];
+            console.log("read pwd = " + valid_password);
         } else {
             console.log('error reading login.config: ' + err);
         }
@@ -95,7 +220,7 @@ function readUser() {
 
 function readDevices() {
     "use strict";
-    //TODO-fertig Lesen Sie die Gerätedaten aus der devices.json Datei ein.
+    //T_ODO-fertig Lesen Sie die Gerätedaten aus der devices.json Datei ein.
     /*
      * Damit die Simulation korrekt funktioniert, müssen Sie diese mit nachfolgender Funktion starten
      *      simulation.simulateSmartHome(devices.devices, refreshConnected);
@@ -131,6 +256,10 @@ var server = app.listen(8081, function () {
     "use strict";
     readUser();
     readDevices();
+
+    var date = new Date();
+    server_start_date = date.getDate() + "." + (date.getMonth()+1) + "." + (date.getFullYear());
+    server_start_time = date.getHours() + ":" + date.getMinutes();
 
     var host = server.address().address;
     var port = server.address().port;
